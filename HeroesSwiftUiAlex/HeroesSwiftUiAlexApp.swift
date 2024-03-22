5f091acf2d9c40feb7ec9b59390d05ab18c5ef9f
alex <alexmaxu12@gmail.com>
2024-03-22 22:46:18 +0100

    Initial Commit

---
+++ HeroesSwiftUiAlex/HeroesSwiftUiAlexApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroesSwiftUiAlexApp.swift
+//  HeroesSwiftUiAlex
+//
+//  Created by Alex  on 22/3/24.
+//
+
+import SwiftUI
+
+@main
+struct HeroesSwiftUiAlexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
